d11a3b3ff3af2d760a7e4d1c4ab9f1eb0bede3bc
Kenneth Kousen <ken.kousen@kousenit.com>
2012-01-07 02:02:31 -0500

    moved integration tests to separate source dir

---
+++ ch10/HelloServlet/src/integrationTest/groovy/mjg/ServletIntegrationTests.groovy
@@ -0,0 +1,21 @@
+package mjg;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+class ServletIntegrationTests {
+    def httpPort = 8163
+    
+    @Test
+    void testHelloServlet() {
+        String response = "http://localhost:$httpPort/HelloServlet/hello".toURL().text.trim()
+        assert response == 'Hello, Servlet!'
+    }
+    
+    @Test
+    void testHelloGroovyServlet() {
+        String response = "http://localhost:$httpPort/HelloServlet/hellogs".toURL().text.trim()
+        assert response == 'Hello from a Groovy Servlet!'
+    }
+}
