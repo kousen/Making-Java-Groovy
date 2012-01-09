3594ff396614d66db48d830937124bd2a374a48a
Kenneth Kousen <ken.kousen@kousenit.com>
2012-01-08 21:10:25 -0500

    Added Java test of servlet

--- ch10/HelloServlet/src/main/java/mjg/HelloServlet.java
+++ ch10/HelloServlet/src/main/java/mjg/HelloServlet.java
@@ -7,10 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class HelloServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
 	    response.getWriter().print("Hello, Servlet!");
-	}
-
+    }
 }
