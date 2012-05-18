74821afe9f3c22b6574de8b0e4215e1985c82f07
Kenneth Kousen <ken.kousen@kousenit.com>
2012-05-18 09:59:17 +0100

    gmaven basic project

---
+++ ch04/maven-project/src/test/groovy/mjg/mvn/ExampleTest.groovy
@@ -0,0 +1,18 @@
+//
+// Generated from archetype; please customize.
+//
+
+package mjg.mvn
+
+import groovy.util.GroovyTestCase
+
+/**
+ * Tests for the {@link Example} class.
+ */
+class ExampleTest
+    extends GroovyTestCase
+{
+    void testShow() {
+        new Example().show()
+    }
+}
---
+++ ch04/maven-project/src/test/groovy/mjg/mvn/HelperTest.groovy
@@ -0,0 +1,18 @@
+//
+// Generated from archetype; please customize.
+//
+
+package mjg.mvn
+
+import groovy.util.GroovyTestCase
+
+/**
+ * Tests for the {@link Helper} class.
+ */
+class HelperTest
+    extends GroovyTestCase
+{
+    void testHelp() {
+        new Helper().help(new Example())
+    }
+}
