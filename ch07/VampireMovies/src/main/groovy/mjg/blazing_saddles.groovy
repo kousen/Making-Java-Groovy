728ef187426e83f4f5bc17fc97c21f161ae518f6
Kenneth Kousen <ken.kousen@kousenit.com>
2012-09-15 14:34:26 -0400

    accessing Mongo (character)

---
+++ ch07/VampireMovies/src/main/groovy/mjg/blazing_saddles.groovy
@@ -0,0 +1,16 @@
+package mjg
+
+import groovy.json.*
+
+String apiKey = '3ufhgmk2h9yf4ksn2mh8zahv'
+def base = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?"
+def qs = [apiKey:apiKey, q: URLEncoder.encode('Blazing Saddles','UTF-8')].collect { it }.join('&')
+def url = "$base$qs"
+def json = new JsonSlurper().parseText(url.toURL().text)
+def movie = json.movies[0]
+// println JsonOutput.prettyPrint(JsonOutput.toJson(movie))
+def allCast = new JsonSlurper().parseText("${movie.links.cast}?apiKey=$apiKey".toURL().text)
+println '---------- All Cast ----------'
+allCast.cast.each { println it }
+println '------------------------------'
+def actor = allCast.cast.find { it.characters =~ /Mongo/ }
