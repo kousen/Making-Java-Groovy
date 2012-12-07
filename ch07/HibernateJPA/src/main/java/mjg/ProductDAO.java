2f0337f7ab92c1af1af129fd7f4eff257771f28d
Kenneth Kousen <ken.kousen@kousenit.com>
2012-12-07 00:57:02 -0500

    some DAO cleanup and fixed an error in the app ctx

--- ch07/HibernateJPA/src/main/java/mjg/ProductDAO.java
+++ ch07/HibernateJPA/src/main/java/mjg/ProductDAO.java
@@ -5,11 +5,7 @@ import java.util.List;
 public interface ProductDAO {
 
 	List<Product> getAllProducts();
-
 	Product findProductById(int id);
-
 	int insertProduct(Product p);
-
 	void deleteProduct(int id);
-
 }
