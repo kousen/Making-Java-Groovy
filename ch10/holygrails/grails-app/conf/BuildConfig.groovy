2f1db45766b9a28ff4682782c7ded68ed22859e3
Kenneth Kousen <ken.kousen@kousenit.com>
2012-12-26 14:35:17 -0500

    upgrade to Grails 2.2

--- ch10/holygrails/grails-app/conf/BuildConfig.groovy
+++ ch10/holygrails/grails-app/conf/BuildConfig.groovy
@@ -61,6 +61,7 @@ grails.project.dependency.resolution = {
         //runtime ":cached-resources:1.0"
         //runtime ":yui-minify-resources:0.1.4"
 
+        compile ':google-visualization:0.6'
         build ":tomcat:$grailsVersion"
     }
 }
