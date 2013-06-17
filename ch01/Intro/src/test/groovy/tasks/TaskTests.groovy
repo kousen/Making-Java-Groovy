7624b39bb88d5ab7a82b1e95502ed4d0ebbff058
Kenneth Kousen <ken.kousen@kousenit.com>
2013-06-17 15:41:55 -0500

    upgrading to groovy 2.1.5

--- ch01/Intro/src/test/groovy/tasks/TaskTests.groovy
+++ ch01/Intro/src/test/groovy/tasks/TaskTests.groovy
@@ -18,22 +18,17 @@ package tasks
 import spock.lang.Specification;
 
 class TaskTests extends Specification {
-    JavaTask jtask
-    GroovyTask gtask
-    
-    def setup() {
-        jtask = new JavaTask(name:'name',priority:1,
+    JavaTask jtask = new JavaTask(name:'name',priority:1,
+        startDate:new Date(),endDate:new Date() + 1)
+
+    GroovyTask gtask = new GroovyTask(name:'name',priority:1,
             startDate:new Date(),endDate:new Date() + 1)
-        
-        gtask = new GroovyTask(name:'name',priority:1,
-            startDate:new Date(),endDate:new Date() + 1)
-    }
     
     def "groovy and java tasks are equivalent"() {
         expect:
         jtask.name == gtask.name
         jtask.priority == gtask.priority
-        Math.abs(jtask.startDate - gtask.startDate) < 1
-        Math.abs(jtask.endDate - gtask.endDate) < 1
+        (jtask.startDate - gtask.startDate).abs() < 1
+        (jtask.endDate - gtask.endDate).abs() < 1
     }
 }
