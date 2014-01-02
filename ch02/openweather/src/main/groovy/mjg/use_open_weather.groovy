de47957b2d8cf4d18e8728720a0fe7303d2b2b15
Kenneth Kousen <ken.kousen@kousenit.com>
2014-01-01 20:36:49 -0500

    using the open weather class

--- ch02/openweather/src/main/groovy/mjg/use_open_weather.groovy
+++ ch02/openweather/src/main/groovy/mjg/use_open_weather.groovy
@@ -1,7 +1,7 @@
 package mjg
 
 OpenWeather ow = new OpenWeather()
-println ow.weather
+println ow.weather  // called Marlborough, CT, but really Hartford
 println ow.getWeather('Brisbane','Australia')
 println ow.getWeather('Paris','France')
 println ow.getWeather('London','UK')
