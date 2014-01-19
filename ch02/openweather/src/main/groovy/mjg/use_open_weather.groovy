4dd050c3b08b6233db3dfb361aec7ba09406c491
Kenneth Kousen <ken.kousen@kousenit.com>
2014-01-19 09:51:15 -0600

    Added sunny in philadelphia joke to open weather demo

--- ch02/openweather/src/main/groovy/mjg/use_open_weather.groovy
+++ ch02/openweather/src/main/groovy/mjg/use_open_weather.groovy
@@ -4,14 +4,17 @@ OpenWeather ow = new OpenWeather()
 println ow.weather  // called Marlborough, CT, but really Hartford
 
 // Home of Paul King, co-author of _Groovy in Action_ and my personal hero
-println ow.getWeather('Brisbane','Australia')
+println ow.getWeather('Brisbane', 'Australia')
 
 // Home of Guillaume Laforge, head of the Groovy project
-// (also one of my heroes, along with Dierk Koenig, Graeme Rocher, ...)
-println ow.getWeather('Paris','France')
+// (also one of my heroes, along with Dierk Koenig, Graeme Rocher, Tom Brady, David Ortiz, ...)
+println ow.getWeather('Paris', 'France')
 
 // Have to check the weather in Java, right?
-// println ow.getWeather('Java','Indonesia')
+println ow.getWeather('Java','Indonesia')
+
+// Is it always sunny in Philadelphia?
+println ow.getWeather('Philadelphia', 'PA')
 
 // Any weather stations in Antarctica?
 println ow.getWeather('', 'Antarctica')
