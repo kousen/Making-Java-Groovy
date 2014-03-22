9cd41a4992fb17b83ffeff949c778ab7d17b1a24
Kenneth Kousen <ken.kousen@kousenit.com>
2014-03-22 12:09:25 -0400

    Updated wrapper and collapsed catch blocks

--- ch03/Integration/src/geocodeV3.groovy
+++ ch03/Integration/src/geocodeV3.groovy
@@ -1,3 +1,4 @@
+// undeclared street, city, state part of binding
 String encoded = [street,city,state].collect {
 	URLEncoder.encode(it,'UTF-8')
 }.join(',')
@@ -5,5 +6,7 @@ String qs = [address: encoded, sensor: false].collect { it }.join('&')
 String base = 'http://maps.googleapis.com/maps/api/geocode/xml?'
 println "$base$qs"
 def root = new XmlSlurper().parse("$base$qs")
+
+// undeclared lat, lng part of binding
 lat = root.result[0].geometry.location.lat.toString()
 lng = root.result[0].geometry.location.lng.toString()
