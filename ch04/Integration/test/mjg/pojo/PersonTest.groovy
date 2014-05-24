af80614376027e3a33a9484a3ad1be306858982b
Kenneth Kousen <ken.kousen@kousenit.com>
2014-05-24 14:25:52 -0400

    switched from JUnit assert* to Groovy asserts

--- ch04/Integration/test/mjg/pojo/PersonTest.groovy
+++ ch04/Integration/test/mjg/pojo/PersonTest.groovy
@@ -25,27 +25,27 @@ class PersonTest {
     @Test
     public void testPerson() {
         p = new Person()
-        assertEquals 0, p.id
-        assertNull p.name
+        assert !p.id
+        assert !p.name
     }
 
     @Test
     public void testPersonIntString() {
         p = new Person(1,'Buffy')
-        assertEquals 1, p.id
-        assertEquals "Buffy", p.name
+        assert 1 == p.id
+        assert 'Buffy' == p.name
     }
 
     @Test
     public void testGetId() {
         p = new Person(id:99)
-        assertEquals 99, p.id
+        assert 99 == p.id
     }
 
     @Test
     public void testGetName() {
         p = new Person(id:86,name:'Maxwell Smart')
-        assertEquals 86, p.id
-        assertEquals 'Maxwell Smart', p.name
+        assert 86 == p.id
+        assert 'Maxwell Smart' == p.name
     }
 }
