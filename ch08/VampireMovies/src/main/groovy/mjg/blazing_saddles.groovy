292a0c642c6ca941d5c86b1d4fab00b9a4fac927
Kenneth Kousen <ken.kousen@kousenit.com>
2014-08-01 14:34:45 -0400

    PrettyPrint Json data

--- ch08/VampireMovies/src/main/groovy/mjg/blazing_saddles.groovy
+++ ch08/VampireMovies/src/main/groovy/mjg/blazing_saddles.groovy
@@ -6,7 +6,7 @@ String apiKey = new File('rotten_tomatoes_apiKey.txt').text
 String base = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?"
 String qs = [apiKey:apiKey, q: URLEncoder.encode('Blazing Saddles','UTF-8')].collect { it }.join('&')
 String url = "$base$qs"
-// println JsonOutput.prettyPrint(url.toURL().text)
+println JsonOutput.prettyPrint(url.toURL().text)
 def json = new JsonSlurper().parseText(url.toURL().text)
 def movie = json.movies[0]
 def allCast = new JsonSlurper().parseText("${movie.links.cast}?apiKey=$apiKey".toURL().text)
