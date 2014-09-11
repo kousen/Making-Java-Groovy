dec663dce20fcb5a5cc7599461cd5fde50206bde
Kenneth Kousen <ken.kousen@kousenit.com>
2014-09-11 09:43:13 -0500

    add single movie queries to vampire demo

--- ch08/VampireMovies/src/main/groovy/mjg/query_db.groovy
+++ ch08/VampireMovies/src/main/groovy/mjg/query_db.groovy
@@ -12,3 +12,7 @@ db.vampireMovies.find(critics_consensus : ~/.*/).each { movie ->
     //println "$movie.id $movie.title : $movie.critics_consensus"
     println Movie.fromJSON(movie)
 }
+
+def bs = db.otherMovies.findOne(title: 'Blazing Saddles')
+println bs
+println Movie.fromJSON(bs)
